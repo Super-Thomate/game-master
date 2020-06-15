74de35bbb084e43d2484faa273ca84b2709eb702
Super-Thomate <tomplaindoux1@orange.fr>
2020-06-15 17:11:29 +0000

    fix

--- cogs/nickname/nickname.py
+++ cogs/nickname/nickname.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 import Utils
 
-from core import logger
+from core import logger, fetch_one_line
 
 class Nickname(commands.Cog):
   def __init__(self, bot):
@@ -21,6 +21,15 @@ class Nickname(commands.Cog):
 
   @nickname.command()
   async def set (self, ctx, nickname: str, voiceChannel: discord.VoiceChannel):
+    """
+    Set a nickname for a specific voice channel (use `"` if you use space in your nickname)
+    """
     guild_id                 = ctx.guild.id
+    user_id                  = ctx.author.id
+    channel_id               = voiceChannel.id
+    select                   = 'select count (*) from `nickname_set` where user_id=? and channel_id=? ;'
+    data                     = fetch_one_line (select, [user_id, channel_id])
+    logger ("nickname::set", "In data: {}".format (data))
+    
     
     
